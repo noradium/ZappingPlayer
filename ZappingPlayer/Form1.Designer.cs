af9d82a27552209b3d62f6ee17c0c250c55cc777
noradium <noradium888@gmail.com>
2014-07-15 23:40:50 +0900

    v1.0
    first commit

---
+++ ZappingPlayer/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace ZappingPlayer
+{
+    partial class ZappingPlayer
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 35);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(347, 29);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(375, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 29);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "開く";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Current Directory";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 145);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(70, 41);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "再生";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(88, 145);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(70, 41);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "一時停止";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(164, 145);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(70, 41);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "停止";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(13, 213);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "再生中ファイル";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.label3.Location = new System.Drawing.Point(120, 213);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(426, 55);
+            this.label3.TabIndex = 7;
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.checkBox1.Location = new System.Drawing.Point(159, 95);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(164, 30);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "Search All Directory";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(425, 95);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(112, 45);
+            this.trackBar1.TabIndex = 10;
+            this.trackBar1.TickFrequency = 10;
+            this.trackBar1.Value = 50;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label4.Location = new System.Drawing.Point(379, 101);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 16);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "音量";
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("MS UI Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button5.Location = new System.Drawing.Point(12, 88);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(130, 41);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "リスト作成";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label5.Location = new System.Drawing.Point(64, 286);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(369, 16);
+            this.label5.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label6.Location = new System.Drawing.Point(13, 286);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 16);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "INFO.";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.richTextBox2.Location = new System.Drawing.Point(425, 146);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(49, 40);
+            this.richTextBox2.TabIndex = 14;
+            this.richTextBox2.Text = "14";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label7.Location = new System.Drawing.Point(270, 155);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(149, 16);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "1曲あたり再生時間[s]";
+            // 
+            // ZappingPlayer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(549, 311);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "ZappingPlayer";
+            this.Text = "ZappingPlayer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ ZappingPlayer/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZappingPlayer
+{
+    public partial class ZappingPlayer : Form
+    {
+        private enum PlayerStatus {Play, Pause, Stop };
+        private PlayerStatus status = PlayerStatus.Stop;
+
+        WMPLib.WindowsMediaPlayer mediaPlayer = new WMPLib.WindowsMediaPlayer();
+
+        private string[] fileArray = null;
+        private SearchOption searchOption = SearchOption.AllDirectories;
+        private Random cRandom = new Random();
+        private int playerVolume;
+        private double playtime = 14.0;
+        private Timer timer = null;
+
+
+        public ZappingPlayer()
+        {
+            InitializeComponent();
+            mediaPlayer.settings.autoStart = false;
+            playerVolume = this.trackBar1.Value;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.folderBrowserDialog1.ShowDialog();
+            this.richTextBox1.Text = this.folderBrowserDialog1.SelectedPath;
+
+            
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (this.checkBox1.Checked == true)
+            {
+                this.searchOption = SearchOption.AllDirectories;
+            }
+            else
+            {
+                this.searchOption = SearchOption.TopDirectoryOnly;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {   // 再生ボタン
+            if (fileArray.Length > 0)
+            {
+                if (status == PlayerStatus.Stop)
+                {
+                    newStart();
+                }
+                else if (status == PlayerStatus.Pause)
+                {
+
+                }
+            }
+        }
+
+        private void timer_tick_FadeIn(object sender, EventArgs e)
+        {
+            if (mediaPlayer.settings.volume < this.playerVolume)
+            {
+                mediaPlayer.settings.volume++;
+            }
+            else
+            {
+                timer.Dispose();
+                timer = new Timer();
+                timer.Tick += new EventHandler(startFadeOut);
+                timer.Interval = (int)(playtime * 1000);
+                timer.Start();
+            }
+
+
+        }
+
+        private void timer_tick_FadeOut(object sender, EventArgs e)
+        {
+            if (mediaPlayer.settings.volume > 0)
+            {
+                mediaPlayer.settings.volume--;
+            }
+            else
+            {
+                timer.Stop();
+                newStart();
+            }
+
+
+        }
+        private void newStart()
+        {
+            if (timer != null)
+            {
+                timer.Dispose();
+            }
+            int fileIndex = cRandom.Next(fileArray.Length);
+            Console.WriteLine("fileIndex = " + fileIndex);
+            mediaPlayer.URL = fileArray[fileIndex];
+            mediaPlayer.controls.play();
+
+            double startPosition = cRandom.Next(120);//(double)cRandom.Next((int)(mediaPlayer.controls.currentItem.duration * 2.0 / 3.0));
+            Console.WriteLine("duration = " + mediaPlayer.currentMedia.duration);
+            Console.WriteLine("startPosition = " + startPosition);
+            mediaPlayer.controls.currentPosition = startPosition;
+            mediaPlayer.settings.volume = 0;
+            
+            timer = new Timer();
+            timer.Tick += new EventHandler(timer_tick_FadeIn);
+            timer.Interval = 80;
+            timer.Enabled = true;
+
+            
+            status = PlayerStatus.Play;
+            label3.Text = mediaPlayer.URL;
+
+        }
+
+        private void startFadeOut(object sender, EventArgs e)
+        {
+            timer.Dispose();
+            timer = new Timer();
+            timer.Tick += new EventHandler(timer_tick_FadeOut);
+            timer.Interval = 80;
+            timer.Start();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {   //　一時停止ボタン
+            if (status == PlayerStatus.Play)
+            {
+                timer.Stop();
+                mediaPlayer.controls.pause();
+                status = PlayerStatus.Pause;
+            }
+            else if(status == PlayerStatus.Pause)
+            {
+                timer.Start();
+                mediaPlayer.controls.play();
+                status = PlayerStatus.Play;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {   // 停止ボタン
+            timer.Dispose();
+            mediaPlayer.controls.stop();
+            status = PlayerStatus.Stop;
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            mediaPlayer.settings.volume = this.trackBar1.Value;
+            this.playerVolume = this.trackBar1.Value;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            fileArray = Directory.GetFiles(this.richTextBox1.Text, "*.mp3", searchOption);
+
+            label5.Text = fileArray.Length + " files are loaded.";
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+            this.playtime = Double.Parse(richTextBox2.Text);
+        }
+
+
+
+
+
+    }
+}
